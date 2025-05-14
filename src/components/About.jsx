c3a740911c3be5fb679c121113feadf38e73ad3e
NathanGrim <samarjitrana66@gmail.com>
2025-05-14 20:32:59 +0530

    Initial commit of 3D web portfolio project

---
+++ src/components/About.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import  {Tilt} from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I am a passionate developer with a diverse skill set and a keen interest in crafting impactful digital experiences. With hands-on experience in PHP, .NET, vanilla HTML, CSS, JavaScript, the MERN stack, and Three.js, I enjoy exploring different technologies and bringing creative ideas to life.
+
+        In addition to web development, I have an academic inclination toward AI and text summarization. My recent paper, focusing on summarization using various models, was accepted for publication, showcasing my ability to blend technical knowledge with research-driven solutions.
+
+        When I'm not coding or researching, I enjoy learning new frameworks, experimenting with 3D visualizations, and staying ahead in the ever-evolving tech landscape.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
